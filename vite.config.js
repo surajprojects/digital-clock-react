0fe8bfdffd7858fbc7835d44b223cc330dde6115
TIGER <suraj23082002@gmail.com>
2024-09-26 09:05:14 +0530

    configured files for first deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/digital-clock-react/',  // This should match your GitHub repo name
   plugins: [react()],
 })
