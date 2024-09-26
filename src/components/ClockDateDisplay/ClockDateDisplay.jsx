c64f42bc2ee98baa871bb34b318064bfd98c0884
TIGER <suraj23082002@gmail.com>
2024-09-26 14:32:00 +0530

    fixed the space issue in clock date display component

--- src/components/ClockDateDisplay/ClockDateDisplay.jsx
+++ src/components/ClockDateDisplay/ClockDateDisplay.jsx
@@ -3,11 +3,6 @@
 
 export default function ClockDateDisplay({ date = 1, month = "Jan", year = 2024, day = "Monday" }) {
     return (
-        <h3 id="clock-date-display">
-            <span>{date < 10 ? "0" + date : date}</span>
-            <span>{month}</span>
-            <span>{year}</span>
-            <span>{day}</span>
-        </h3>
+        <h3 id="clock-date-display"><span>{date < 10 ? "0" + date : date}</span> <span>{month}</span> <span>{year}</span> <span>{day}</span></h3>
     );
 };
--- src/components/ClockTimeDisplay/ClockTimeDisplay.jsx
+++ src/components/ClockTimeDisplay/ClockTimeDisplay.jsx
@@ -3,10 +3,6 @@
 
 export default function ClockTimeDisplay({ hrs = 0, mins = 0, secs = 0 }) {
     return (
-        <h2 id="clock-time-display">
-            <span>{hrs < 10 ? "0" + hrs : hrs}</span>:
-            <span>{mins < 10 ? "0" + mins : mins}</span>:
-            <span>{secs < 10 ? "0" + secs : secs}</span>
-        </h2>
+        <h2 id="clock-time-display"><span>{hrs < 10 ? "0" + hrs : hrs}</span>:<span>{mins < 10 ? "0" + mins : mins}</span>:<span>{secs < 10 ? "0" + secs : secs}</span></h2>
     );
 };
